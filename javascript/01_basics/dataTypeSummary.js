44a33e96424c4e3610054e81db391525ba593bfd
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-05 06:38:26 +0000

    data types summary

--- javascript/01_basics/dataTypeSummary.js
+++ javascript/01_basics/dataTypeSummary.js
@@ -1,1 +1,17 @@
-  
+  // data types
+
+//primitive-number,symbol,string,boolean,bigint, undefined,null
+
+//non - primitive--> array object function
+
+const heros = ["tom", "wool", "add"];
+let obj = {
+  name: "adarsh",
+  age: 34,
+};
+
+const myfunction = function () {
+  console.log("i am adarsh");
+};
+
+myfunction();
