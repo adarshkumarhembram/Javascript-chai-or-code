24db815ce188b63b066618161532affb8ba6a395
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-06 10:53:35 +0000

    some maths

--- javascript/01_basics/06_numAndDates.js
+++ javascript/01_basics/06_numAndDates.js
@@ -12,3 +12,13 @@ console.log(othernumber.toPrecision(2));
 
 const hundeds=1000;
 console.log(hundeds.toLocaleString('en-in'));
+
+//  maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(7,5,3,2));
