68bed316f67731e1ff4391e8677f5ac8427598aa
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-06 05:48:07 +0000

    string

---
+++ javascript/01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name = "Adarsh";
+const repocount = 50;
+
+// console.log(name + repocount + " value");
+// console.log(`my name is ${name} and my repo is ${repocount}`);
+
+const gameName = new  String('adarsh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
