c7e8752c076f62d08f6533a94ca23b268215e980
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-07 16:44:39 +0000

    array

---
+++ javascript/01_basics/08_array.js
@@ -0,0 +1,15 @@
+// array
+
+const myarr  =[1,2,3,4,4]
+const myheros = ["shaktiman","bhim","raju"]
+
+const myarr2= new Array( 1,2,3,4)
+console.log(myarr[1]);
+
+
+// array methods
+
+myarr.push(7)
+myarr.pop(7)
+myarr.unshift(7)
+console.log(myarr);
