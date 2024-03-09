4937156859146e2add137a9200d0a92491a58353
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-09 14:41:40 +0000

    object email

--- 02_object/01.js
+++ 02_object/01.js
@@ -4,7 +4,9 @@ const jsuser={
     name: "Adarsh",
     age: 22,
     location: "dhanbad",
+    email: "uhyg@gmail.com"
 
 }
 console.log(jsuser);
 console.log(jsuser.age);
+console.log(jsuser.email);
