66b017e4c1fb56917f28221244b14568b392e6ab
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-11 16:46:06 +0000

    call

--- 03_function/1.js
+++ 03_function/1.js
@@ -23,3 +23,18 @@ function addTwonumbers(number1,number2){
     console.log(number1+number2);
 }
 addTwonumbers(4,null)
+
+
+function one (){
+    const username = "Adarsh"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    console.log(website);
+
+    two()
+}
+
+one()
