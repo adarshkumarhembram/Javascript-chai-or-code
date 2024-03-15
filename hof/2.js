3ebaebf5210feda5bfeb2f7f05a2cf385555d709
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-15 12:13:23 +0000

    map

--- hof/2.js
+++ hof/2.js
@@ -7,3 +7,11 @@ arr.forEach(function(element,index,arr){
 arr.forEach((element,index,arr)=>{
     console.log("arrows:",index,element,arr);
 })
+
+
+const heros=["a","b","c"]
+heros.forEach((el) =>{console.log(el.toUpperCase());})
+
+heros.map(function(element,index,heros){
+    console.log(index,element,heros);
+})
