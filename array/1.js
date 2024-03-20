5e1e2429fba983bbf9d23df2886d9f59fe261052
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-20 15:00:19 +0000

    array

---
+++ array/1.js
@@ -0,0 +1,10 @@
+const user = {
+    username: "adarsh",
+    price: 999,
+
+    welcomeMessege: function(){
+        console.log(`${this.userbane} ,welcome to website `);
+    }
+}
+
+user.welcomeMessege()
