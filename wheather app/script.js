f8ef006779a02117e931e246df4f9f4774dda5b1
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-03-24 16:33:19 +0000

    pro

---
+++ wheather app/script.js
@@ -0,0 +1,10 @@
+async function getData(event) {
+    event.preventDefault();
+  
+    const response = await fetch(
+      `http://api.weatherstack.com/current?access_key=ee86bb84089fcd5c67f0d51ddb347e23&query=jaipur`
+    );
+  
+    console.log(response);
+  }
+  
