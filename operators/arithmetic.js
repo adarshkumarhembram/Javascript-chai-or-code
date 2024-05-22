fc1254e086bffe53dc1888eb0907db1d3f727400
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-22 14:57:35 +0000

    y

--- operators/arithmetic.js
+++ operators/arithmetic.js
@@ -14,3 +14,13 @@ console.log(x=y);
 //comparision operator
 
 console.log(x>y);
+
+// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+let age = prompt("Enter your age");
+
+if (age >= 18) {
+  console.log("you are old enough to drive");
+} else {
+  console.log(`${18 - age} years he need to turn 18`);
+}
