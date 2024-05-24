2fc96a7daba35851b580e36c10bb5d08113de5a4
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-24 13:50:04 +0000

    practice some question

--- javascript/01_basics/01.js
+++ javascript/01_basics/01.js
@@ -23,3 +23,13 @@ console.table([accoundId, accountEmail, accountPassword, accountPassword]);
 // prefer not to use var beacuse issue in block scope and function scope
 //
 console.log("hello");
+
+// 13. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+let age = prompt("Enter your age");
+
+if (age >= 18) {
+  console.log("you are old enough to drive");
+} else {
+  console.log(`${18 - age} years he need to turn 18`);
+}
