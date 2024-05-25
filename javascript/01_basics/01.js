923da00f96a1964a28abfe39e2e2c9fe8354f953
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-25 07:31:21 +0000

    basic

--- javascript/01_basics/01.js
+++ javascript/01_basics/01.js
@@ -33,3 +33,14 @@ if (age >= 18) {
 } else {
   console.log(`${18 - age} years he need to turn 18`);
 }
+
+// Write a javascript program which takes the input of filename as string and prints the extension of the file in the console.
+
+const getFileExtension = (fileName) => {
+  const fileNameArray = fileName.split(".");
+  const fileExtension = fileNameArray[fileNameArray.length - 1];
+
+  console.log(fileExtension);
+};
+
+getFileExtension("example.java"); // prints "txt" in the console
