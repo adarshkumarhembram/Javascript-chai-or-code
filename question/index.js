fd826ff259d43d60ec2c67800cbb9f6572de8209
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-29 21:20:17 +0000

    js

--- question/index.js
+++ question/index.js
@@ -81,3 +81,14 @@ for (let i = 1; i <= 5; i++) {
 
   console.log(bag);
 }
+
+// Write a javascript program which takes the input of filename as string and prints the extension of the file in the console.
+
+const getFileExtension = (fileName) => {
+  const fileNameArray = fileName.split(".");
+  const fileExtension = fileNameArray[fileNameArray.length - 1];
+
+  console.log(fileExtension);
+};
+
+getFileExtension("example.java"); // prints "txt" in the console
