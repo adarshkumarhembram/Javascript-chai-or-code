6ce330261bf18f8bb7449f038acf59d07dbc91f1
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-31 10:35:47 +0000

    js

--- question/index.js
+++ question/index.js
@@ -93,3 +93,22 @@ const getFileExtension = (fileName) => {
 
 getFileExtension("example.java"); // prints "txt" in the console
 // console.log();
+
+
+// Create a simple calculator program in JavaScript which can perform the addition, substraction, multiplication and division on given numbers.
+
+function addition(a, b) {
+  return a + b;
+}
+
+function substraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
