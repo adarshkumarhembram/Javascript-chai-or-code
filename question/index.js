8102b400ca2eadb74954c39934ccf66c8ae5af72
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-06-02 16:01:28 +0000

    ques

--- question/index.js
+++ question/index.js
@@ -113,3 +113,18 @@ function division(a, b) {
   return a / b;
 }
 // console.log();
+
+
+// . Write a program to print the table of any number given by the user. The format of the output should be smiliar to the below example-
+// 	If the number given by the user is 2 then the output should look like this-
+// 	2 * 1 = 2
+// 	2 * 2 = 4
+// 	2 * 3 = 6 and so on till 2 * 10 = 20.
+
+let nume= 5;
+let i = 1;
+
+while (i <= 10) {
+  console.log(i * nume);
+  i++;
+}
