546f0a0540088884ec4782634120f929c44ef31b
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-06-03 17:36:38 +0000

    QUES

--- question/index.js
+++ question/index.js
@@ -128,3 +128,21 @@ function division(a, b) {
 //   console.log(i * nume);
 //   i++;
 // }
+
+// . In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+let contries = ["INDIA", "BANGLADESH", "AUSTRALIA", "ETHIOPIA"];
+
+let flag = true;
+for (let i = 0; i < contries.length; i++) {
+  if (contries[i] == "ETHIOPIA") {
+    console.log("ETHIOPIA");
+    break;
+  } else {
+    flag = false;
+  }
+}
+
+if (flag == false) {
+  contries.push("Ethiopia");
+}
