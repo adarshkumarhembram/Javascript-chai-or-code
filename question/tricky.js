dcfa94f3247c0a75d8b2772c4c9fca71b6f3418e
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-06-17 15:41:42 +0000

    js

--- question/tricky.js
+++ question/tricky.js
@@ -75,4 +75,4 @@ console.log(1 + +'2' + '2'); // Output: "32"
 console.log(1 + -'1' + '2'); // Output: "02"
 console.log(+'1' + '1' + '2'); // Output: "112"
 console.log('A' - 'B' + '2'); // Output: "NaN2"
-console.log('A' - 'B' + 2); // Output: NaN
+console.log('A' - 'B' + 2); // Output: NaN/
