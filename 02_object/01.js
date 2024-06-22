23ed14421055f52dbb95a5c3fedac4d4b549c795
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-06-22 17:28:46 +0000

    object

--- 02_object/01.js
+++ 02_object/01.js
@@ -11,3 +11,19 @@ console.log(jsuser);
 console.log(jsuser.age);
 console.log(jsuser.email);
 console.log(jsuser["email"]);
+
+
+// Creating an object using object literal syntax
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+// Accessing properties and methods
+console.log(person.firstName);  // Output: John
+console.log(person.age);        // Output: 30
+console.log(person.fullName()); // Output: John Doe
