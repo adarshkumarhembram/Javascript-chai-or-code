1a139e5f6c24d340a0334ca800b43bda7f3bf9a2
adarshkumarhembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-06-30 18:02:19 +0000

    some tricky concept

--- wheather app/script.js
+++ wheather app/script.js
@@ -8,3 +8,15 @@ async function getData(event) {
     console.log(response);
   }
   // none
+
+
+  function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  console.log(factorial(5)); // Calculate factorial of 5
+  
